eb098adbe386ac6420c762319b0daef983798497
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2026-01-17 20:02:56 -0500

    feat: add dialog component for creating new files with input and footer actions

--- src/app/dashboard/files/page.tsx
+++ src/app/dashboard/files/page.tsx
@@ -1,12 +1,51 @@
-import { TableView } from "@/components/table-view"
+import { TableView } from "@/components/table-view";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Files = () => {
   return (
     <div className="flex flex-col w-full max-w-4xl h-full mx-auto">
-      <div className="text-4xl mb-5 font-(family-name:--font-instrument-serif)">Files</div>
+      <div className="flex flex-row justify-between items-center">
+        <div className="text-4xl mb-5 font-(family-name:--font-instrument-serif)">
+          Files
+        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>New File</Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create New File</DialogTitle>
+              <DialogDescription>
+                Create a new transcription file.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="file-name" className="mt-4">
+                File Name
+              </Label>
+              <Input placeholder="File Name" className="w-full mb-4" />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Create File</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
       <TableView />
     </div>
-  )
-}
+  );
+};
 
-export default Files
+export default Files;
